36752a679d7e2291177b2129a3fbe3d90fb0c2ef
Danny Fang <xuqfang@microsoft.com>
2022-04-25 14:27:35 +0800

    commit for configuration of jdk11

---
+++ src/com/xuqfang/test/TestHashMap.java
@@ -0,0 +1,11 @@
+package com.xuqfang.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestHashMap {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("K1", 1);
+    }
+}
